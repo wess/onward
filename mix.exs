2b626d982c767c58814eb460b508fb4412849bed
Wess Cope <wess@wess.local>
2024-07-24 09:10:34 -0400

    Preparing for hex package

--- mix.exs
+++ mix.exs
@@ -1,14 +1,28 @@
 defmodule Onward.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/wess/onward"
+  @description "A Plug to proxy requests to another location"
+  @version "0.0.1"
+
   def project do
     [
       app: :onward,
-      version: "0.0.1",
+      description: @description,
+      version: @version,
+      package: package(),
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      maintainers: ["Wess Cope"],
+      links: %{"Github" => @source_url}
+    }
   end
 
   # Run "mix help compile.app" to learn about applications.
@@ -23,6 +37,7 @@ defmodule Onward.MixProject do
     [
       {:plug_cowboy, "~> 2.7.1"},
       {:httpoison, "~> 2.2.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 end
